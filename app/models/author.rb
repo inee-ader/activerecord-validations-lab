4783290eee08e1f1ffa74d9f0ca16e4e41a1959e
xoliio <xoliio@gmail.com>
2020-11-01 13:45:17 -0800

    magic with Jesse and Michael

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+
+    validates :name, presence: true, uniqueness: true
+    validates :phone_number, length: { only_integer: true, is: 10 }
+    
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true 
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} must be Fiction or Non-Fiction" }
+    validate :clickbait_title
+
+    ARR = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+    def clickbait_title
+        if ARR.none?{|pattern| pattern.match title}
+            errors.add(:title, "Title must include click-bait phrases")
+        end
+    end
+
+    # def clickbait_title 
+    #     if number.integer? == true && !title.include?("Won't Believe", "Secret", "Top #{number}", "Guess")
+    #         errors.add(:title, "Title must include click-bait phrases")
+    #     end
+    # end
 end
